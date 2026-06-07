b601a6d2bbe5dbcd30675d1b6b01833bbda673fc
lavinicopque <laviniacopque@gmail.com>
2026-06-06 23:06:26 -0300

    Atualização das seguintes  telas

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -74,7 +74,7 @@ void Game::begin() {
     winScreen = new WinScreen(tft);
     gameOverScreen = new GameOverScreen(tft);
 
-    changeState(GameState::LEVEL3);
+    changeState(GameState::INTROLV4);
 }
 
 // =====================================
